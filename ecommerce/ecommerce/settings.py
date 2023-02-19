49a7c16a32e068c0fc868159619afb2ad65e34f5
JacobItopa <sanijacobs@gmail.com>
2023-02-19 17:09:45 +0100

    home view

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -2,10 +2,10 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.ENV()
+env = environ.Env()
 
 # read environment variables
-environ.ENV.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +119,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static_root')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media_root')]
 
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -3,13 +3,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home')
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(setting.STATIC_URL, document_root = setting.STATIC_ROOT)
-    urlpatterns += static(setting.MEDIA_URL, document_root = setting.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
